779bbba5d7a1b9b840490b73a545a08e3d24ab96
ssangmun-whygari <whygari4321@gmail.com>
2026-08-05 23:53:03 +0900

    [Passed] 사각형 채우기 (127ms, 10MB)

---
+++ 코드트리/trail4/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] dp = new int[1000 + 1];
+        dp[1] = 1; dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+
+        System.out.print(dp[N]);
+    }
+}
